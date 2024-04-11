d06d34ed33126888ac3010af212f6a1483014206
Minh Pham <pcminh0505@gmail.com>
2024-04-11 15:56:37 +0700

    chore: Update latest packages

--- your-feast-ui/src/index.js
+++ your-feast-ui/src/index.js
@@ -6,24 +6,24 @@ import FeastUI from "@feast-dev/feast-ui";
 import "@feast-dev/feast-ui/dist/feast-ui.css";
 // import CustomComponent from "./custom-component";
 
-const BASE_URL = process.env.REACT_APP_HOST_URL || "localhost:8000";
+// const BASE_URL = process.env.REACT_APP_HOST_URL || "localhost:8000";
 
 ReactDOM.render(
   <React.StrictMode>
     <FeastUI
-      feastUIConfigs={{
-        projectListPromise: fetch(`${BASE_URL}/projects`, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((response) => {
-            return response.json();
-          })
-          .catch((error) => {
-            console.log(error);
-          }),
-      }}
+    // feastUIConfigs={{
+    //   projectListPromise: fetch(`${BASE_URL}/projects`, {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   })
+    //     .then((response) => {
+    //       return response.json();
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     }),
+    // }}
     />
   </React.StrictMode>,
   document.getElementById("root")
